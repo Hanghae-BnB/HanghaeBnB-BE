f47de50f1f564857d2fc5aa81221b57fb500f978
Seyoung Oh <osy9536@naver.com>
2023-03-03 22:59:36 +0900

    Feat : 추가 Timestamped 로직 #20

--- src/main/java/com/sparta/hanghaebnb/entity/House.java
+++ src/main/java/com/sparta/hanghaebnb/entity/House.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class House {
+public class House extends Timestamped{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/sparta/hanghaebnb/entity/Review.java
+++ src/main/java/com/sparta/hanghaebnb/entity/Review.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Review {
+public class Review extends Timestamped{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
