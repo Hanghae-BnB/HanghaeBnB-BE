5470c713b3d7e859a3f7e4d5ba7109ca1bb496cb
Seyoung Oh <osy9536@naver.com>
2023-03-05 01:14:42 +0900

    Style : 수정 SignupRequestDto 유효성 검사

--- src/main/java/com/sparta/hanghaebnb/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/hanghaebnb/dto/SignupRequestDto.java
@@ -6,7 +6,6 @@ import javax.validation.constraints.Pattern;
 
 @Getter
 public class SignupRequestDto {
-    @Pattern(regexp ="^([a-z[0-9]]){4,10}$", message = "아이디는 4자 이상의 영소문자, 숫자만 가능합니다.")
     private String username;
     @Pattern(regexp ="^([a-zA-Z[0-9]]){8,15}$", message = "비밀번호는 8자 이상의 영대소문자, 숫자만 가능합니다.")
     private String password;
