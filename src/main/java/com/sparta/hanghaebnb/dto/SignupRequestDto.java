73973c7c8b8d0935dc68cbc8062fe423c4f7144c
Seyoung Oh <osy9536@naver.com>
2023-03-06 15:40:25 +0900

    Fix : 수정 email 유효성 검사 구현

--- src/main/java/com/sparta/hanghaebnb/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/hanghaebnb/dto/SignupRequestDto.java
@@ -9,6 +9,8 @@ public class SignupRequestDto {
     private String username;
     @Pattern(regexp ="^([a-zA-Z[0-9]]){8,15}$", message = "비밀번호는 8자 이상의 영대소문자, 숫자만 가능합니다.")
     private String password;
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$"
+            , message = "이메일 형식에 맞지 않습니다.")
     private String email;
     private String birth;
 }
