fb8e5e562705f5049b38d688dc255a27ade43d6d
Seyoung Oh <osy9536@naver.com>
2023-03-08 01:18:41 +0900

    Feat : swagger operation 추가

--- src/main/java/com/sparta/hanghaebnb/controller/UserController.java
+++ src/main/java/com/sparta/hanghaebnb/controller/UserController.java
@@ -7,6 +7,7 @@ import com.sparta.hanghaebnb.dto.request.SignupRequestDto;
 import com.sparta.hanghaebnb.response.ApiDocumentResponse;
 import com.sparta.hanghaebnb.security.UserDetailsImpl;
 import com.sparta.hanghaebnb.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -22,24 +23,19 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UserService userService;
-
+    @Operation(summary = "회원 가입 요청", description = "회원을 가입합니다.", tags = {"Users"})
     @PostMapping("/signup")
     public MessageResponseDto signup(@Valid @RequestBody SignupRequestDto signupRequestDto) {
         return userService.signup(signupRequestDto);
     }
-
+    @Operation(summary = "로그인 요청", description = "회원 로그인 진행", tags = {"Users"})
     @PostMapping("/login")
     public LoginResponseDto login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
         return userService.login(loginRequestDto, response);
     }
-
+    @Operation(summary = "로그아웃 요청", description = "회원 로그아웃 진행", tags = {"Users"})
     @PostMapping("/logout")
     public MessageResponseDto logout(@AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletRequest request) {
         return userService.logout(userDetails.getUser(), request);
-    }
-
-    @PostMapping("/refresh")
-    public MessageResponseDto refresh(HttpServletRequest request, HttpServletResponse response) {
-        return userService.refresh(request, response);
     }
 }
