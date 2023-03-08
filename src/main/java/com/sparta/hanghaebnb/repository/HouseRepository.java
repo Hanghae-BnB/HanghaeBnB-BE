9468e72c792c3168da2ef42d5f1a5e96c7178338
YeowonKIM <kkyoo152@gmail.com>
2023-03-09 04:40:26 +0900

    Feat: 페이징 기능 추가 #87

--- src/main/java/com/sparta/hanghaebnb/repository/HouseRepository.java
+++ src/main/java/com/sparta/hanghaebnb/repository/HouseRepository.java
@@ -1,6 +1,9 @@
 package com.sparta.hanghaebnb.repository;
 
 import com.sparta.hanghaebnb.entity.House;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,9 +11,10 @@ import java.util.List;
 public interface HouseRepository extends JpaRepository<House,Long> {
 
     //생성일 내림차순 전체 조회
-    List<House> findAllByOrderByCreatedAtDesc();
+//    List<House> findAllByOrderByCreatedAtDesc();
+    Page<House> findAllByOrderByCreatedAtDesc(Pageable pageable);
     //카테고리에 따른 전체 여행지 검색
-    List<House> findAllByHouseCaseOrderByCreatedAtDesc(String houseCase);
+    Page<House> findAllByHouseCaseOrderByCreatedAtDesc(String houseCase, Pageable pageable);
     //제목에 키워드가 포함되어 있는 전체 여행지 검색
-    List<House> findAllByTitleContainsOrderByCreatedAtDesc(String category);
+    Page<House> findAllByTitleContainsOrderByCreatedAtDesc(String keyword, Pageable pageable);
 }
